a67e0fc22d32374cdb7b3a8747e294f2714b617f
Urbi Mangal <urbimangal@gmail.com>
2025-12-16 14:55:08 +0530

    Implement single number algorithm using XOR

---
+++ bit manipulation/single_number_1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n=nums.size();
+        int xor1=0;
+        for(int i=0;i<n;i++){
+            xor1=xor1^nums[i];
+        }
+        return xor1;
+    }
+};
