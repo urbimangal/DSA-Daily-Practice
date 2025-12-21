9f97f39bda6fd576c19ee486b05390206ad30c65
Urbi Mangal <urbimangal@gmail.com>
2025-12-22 02:38:25 +0530

    Add prefix to infix conversion function
    Implement function to convert prefix expression to infix.

---
+++ stack/prefix_to_infix.cpp
@@ -0,0 +1,25 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string preToInfix(string s) {
+        // Write your code here
+        int n=s.length();
+        int i=n-1;
+        stack<string> st;
+        while(i>=0){
+            if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
+                st.push(string(1,s[i]));
+            } else{
+                string t1=st.top();
+                st.pop();
+                string t2=st.top();
+                st.pop();
+                string op='('+t1+s[i]+t2+')';
+                st.push(op);
+            }
+            i--;
+        }
+        return st.top();
+    }
+};
