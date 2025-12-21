e6c4356d8ed83eb4f53267a7702ffd83e381c226
Urbi Mangal <urbimangal@gmail.com>
2025-12-22 01:23:05 +0530

    Add infix to postfix conversion method
    Implement infix to postfix conversion using a stack.

---
+++ stack/infix_to_postfix.cpp
@@ -0,0 +1,43 @@
+class Solution {
+  public:
+    int priority(char ch){
+        if(ch=='^') return 3;
+        else if(ch=='*' || ch=='/') return 2;
+        else if(ch=='+' || ch=='-') return 1;
+        else return -1;
+    }
+    string infixToPostfix(string& s) {
+        // code here
+        int n=s.length();
+        int i=0;
+        stack<char> st;
+        string ans="";
+        while(i<n){
+            if((s[i]>='A' && s[i]<='Z') || (s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9')){
+                ans+=s[i];
+            }
+            else if(s[i]=='('){
+                st.push(s[i]);
+            } 
+            else if(s[i]==')'){
+                while(!st.empty() && st.top()!='('){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.pop();
+            } else{
+                while(!st.empty() && (priority(s[i])<priority(st.top()) || (priority(s[i])==priority(st.top()) && s[i]!='^'))){
+                    ans+=st.top();
+                    st.pop();
+                }
+                st.push(s[i]);
+            }
+            i++;
+        }
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        }
+        return ans;
+    }
+};
